12ee59a7004eb16158b9c1c61e7ed0545422222a
climenty <climenty@gmail.com>
2016-10-16 22:49:41 +0300

    Version 1.1.0

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # SVG-Join
 
-There are webpack or gulp tools for combining SVG. But I could not find simple CLI version. Here it is. It just join SVG files in symbol collection with CSS description.
+There are webpack or gulp solutions for combining SVG. But I could not find simple CLI version. Here it is. It just join SVG files in symbol collection with CSS description.
 
 I am strongly recommend to use [svgo](https://github.com/svg/svgo) for optimizing before joining.
 
@@ -10,20 +10,49 @@ I am strongly recommend to use [svgo](https://github.com/svg/svgo) for optimizin
 
 ## Options
 
-* -s, --source  the source directory with filename mask in glob format.
-* -o, --output  the output directory with basename for bundles (svg & css), (*default: "svg-bundle"*).
-* -p, --prefix  prefix for CSS selectors, (*default: "svg_"*).
+Option | Description | Default value
+--- | --- | ---
+-s, --source | the source directory with filename mask in glob format |
+-o, --output | the output directory | "."
+-n, --name | file name (without ext.) for bundles (SVG & CSS) | "svg-bundle"
+--cssName | file name (with ext.) for CSS bundle (if different) |
+-p, --prefix | prefix for CSS selectors | "svg_"
+-m, --mono | extract presentation attributes from single-styled SVG to CSS | false
+--calcSide | calculate omitted side from viewBox values | false
 
-## Examples
+## Usage
 
-`svg-join -s "./svg/*.svg" -o ./public/mybundle`
+**svg-join -s "./svg/*.svg" -o ./public -n mybundle**
 
 Will create mybundle.svg and mybundle.css in public folder.
 
 **Warning!** The files are overwritten silently.
 
-`svg-join -s "/your/path/**/*.svg"`
+**svg-join -s "/your/path/\*\*/*.svg"**
 
 Looking for SVG files recursively in /your/path/ subfolders.
 
+**svg-join -s "./svg/*.svg" --mono**
 
+`<svg id="exmpl"><path... fill="transparent" stroke="#fff" stroke-width="2"/></svg>`
+
+The 'fill', 'stroke', 'stroke-width' attributes will be cutted from SVG and placed into CSS.
+
+`.svg_exmpl {
+  fill: transparent;
+  stroke: #fff;
+  stroke-width: 2;
+}`
+
+**svg-join -s "./svg/*.svg" --calcSide**
+
+`<svg id="f033" height="1.1em" viewBox="0 0 1024 1792">...</svg>`
+
+The omitted side (width in current example) will be calculated from viewBox values.
+
+`.svg_f033 {
+  width: 0.6286em;
+  height: 1.1em;
+}`
+
+*This option applies only for 'em', 'rem' and 'px' units.*
--- svg-join.js
+++ svg-join.js
@@ -8,9 +8,47 @@ const xmldoc = require('xmldoc')
 const path = require('path')
 
 const addPX = x => isFinite(x) ? x + 'px' : x
+const parseUnit = (value = '') => value.replace(/[\d.\s]/g, '')
 
 function errOut (fname, message) {
   console.error(`File: ${fname}\n${message}`)
+}
+
+function lookslike (src, trg, filter = []) {
+  if (filter.length === 0) filter = Object.keys(src)
+  return filter.every(x => src[x] && src[x] === trg[x])
+}
+
+function equals (src, trg) {
+  const filter = Object.keys(src)
+  return filter.length === Object.keys(trg).length && filter.every(x => src[x] === trg[x])
+}
+
+// obj - object, filter - array or object
+function deletelike (obj, filter) {
+  if (!Array.isArray(filter)) filter = Object.keys(filter)
+  filter.forEach(x => delete obj[x])
+}
+
+function style_keys (str) {
+  let items = str.match(/\s?[\w-]+:.*?(;|$)/g)
+  return items.map(x => x.split(':')[0].trim())
+}
+
+// names - array, attr - object
+function style_format (names, attr) {
+  let pad = '  '
+  let head = '.' + names.join(',\n.') + ' {\n'
+  let body = Object.keys(attr).map(x => `${pad}${x}: ${attr[x]};\n`).join('')
+  return head + body + '}\n'
+}
+
+// style - array, attr - object
+function wipe_style_format (style, attr) {
+  return style_format(style.map(x => {
+    deletelike(x.attr, attr)
+    return x.name
+  }), attr)
 }
 
 console.log(`SVG-Join ${require('./package.json').version} Join svg files in symbol collection.`)
@@ -18,23 +56,43 @@ const argv = require('yargs')
   .options({
     's': { alias: 'source', type: 'string', demand: true,
       describe: 'the source directory with filename mask in glob format' },
-    'o': { alias: 'output', default: 'svg-bundle',
-      describe: 'the output directory with basename for bundles (svg & css)' },
-    'p': { alias: 'prefix', default: 'svg_',
-      describe: 'prefix for CSS selectors' }
+    'o': { alias: 'output', type: 'string', default: '.',
+      describe: 'the output directory' },
+    'n': { alias: 'name', type: 'string', default: 'svg-bundle',
+        describe: 'file name (without ext.) for bundles (SVG & CSS)' },
+    'cssName': { type: 'string',
+        describe: 'file name (with ext.) for CSS bundle (if different)' },
+    'p': { alias: 'prefix', type: 'string', default: 'svg_',
+      describe: 'prefix for CSS selectors' },
+    'm': { alias: 'mono', type: 'boolean', default: false,
+      describe: 'extract presentation attributes from single-styled SVG to CSS' },
+    'calcSide': { type: 'boolean', default: false,
+      describe: 'calculate omitted side from viewBox values' }
   })
+  .example('svg-join -s "./svg/*.svg" -o ./public -n mybundle',
+    'Will create mybundle.svg and mybundle.css in public folder.')
   .example('svg-join -s "/your/path/**/*.svg"',
     'Find SVG files in subfolders.')
-  .example('svg-join -s "./svg/*.svg" -o ./public/mybundle',
-    'Will create mybundle.svg and mybundle.css in public folder.')
+  .strict()
   .argv
 
-const svgout = argv.output + '.svg'
-const cssout = argv.output + '.css'
+const svgout = path.join(argv.output, argv.name + '.svg')
+const cssout = path.join(argv.output, argv.cssName || argv.name + '.css')
 const header =
 `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="display:none">
 `
-const preserve = new Set(['viewbox'])
+const preserve = new Set(['viewbox', 'preserveaspectratio'])
+const presentation = new Set(['alignment-baseline', 'baseline-shift', 'clip', 'clip-path', 'clip-rule', 'color',
+  'color-interpolation', 'color-interpolation-filters', 'color-profile', 'color-rendering', 'cursor', 'direction',
+  'display', 'dominant-baseline', 'enable-background', 'fill', 'fill-opacity', 'fill-rule', 'filter', 'flood-color',
+  'flood-opacity', 'font-family', 'font-size', 'font-size-adjust', 'font-stretch', 'font-style', 'font-variant',
+  'font-weight', 'glyph-orientation-horizontal', 'glyph-orientation-vertical', 'image-rendering', 'kerning',
+  'letter-spacing', 'lighting-color', 'marker-end', 'marker-mid', 'marker-start', 'mask', 'opacity', 'overflow',
+  'pointer-events', 'shape-rendering', 'stop-color', 'stop-opacity', 'stroke', 'stroke-dasharray', 'stroke-dashoffset',
+  'stroke-linecap', 'stroke-linejoin', 'stroke-miterlimit', 'stroke-opacity', 'stroke-width', 'text-anchor',
+  'text-decoration', 'text-rendering', 'unicode-bidi', 'visibility', 'word-spacing', 'writing-mode'])
+const units = new Set(['em', 'rem', 'px'])
+const round = new Set(['px'])
 const encoding = 'utf8'
 let symbols = []
 let total = 0
@@ -50,13 +108,57 @@ glob.globAsync(argv.source, { nocase: true }).filter(x => x !== svgout).map(fnam
         if (doc.name.toLowerCase() !== 'svg') {
           throw new Error('Error! The root element must be SVG.')
         }
+
+        let width = 'auto'
+        let height = 'auto'
+        if (doc.attr.viewBox) {
+          let vbox = doc.attr.viewBox.split(/\s+/)
+          if (vbox.length === 4) {
+            width = +vbox[2] - vbox[0]
+            height = +vbox[3] - vbox[1]
+
+            if (argv.calcSide) {
+              let w = parseFloat(doc.attr.width)
+              let wu = parseUnit(doc.attr.width)
+              let h = parseFloat(doc.attr.height)
+              let hu = parseUnit(doc.attr.height)
+              if (!!w && units.has(wu) && !h) {
+                h = height / width * w
+                h = round.has(wu) ? Math.round(h) : h.toFixed(4)
+                doc.attr.height = h + wu
+              } else if (!!h && units.has(hu) && !w) {
+                w = width / height * h
+                w = round.has(hu) ? Math.round(w) : w.toFixed(4)
+                doc.attr.width = w + hu
+              }
+            }
+          }
+        }
+        const rule = {
+          attr: { width: addPX(doc.attr.width || width), height: addPX(doc.attr.height || height) }
+        }
+
         doc.name = 'symbol'
-        const rule = { width: addPX(doc.attr.width), height: addPX(doc.attr.height) }
         Object.keys(doc.attr).forEach(x => {
           if (!preserve.has(x.toLowerCase())) delete doc.attr[x]
         })
-        doc.attr.id = path.basename(fname, path.extname(fname))
+        doc.attr.id = path.basename(fname, path.extname(fname)).replace(/\s/g, '_')
         rule.name = argv.prefix + doc.attr.id
+
+        if (argv.mono) {
+          const styled_children = doc.children.filter(x => {
+            let keys = Object.keys(x.attr)
+            if (x.attr.style) keys = keys.concat(style_keys(x.attr.style))
+            return keys.some(y => presentation.has(y))
+          })
+          if (styled_children.length === 1) {
+            Object.keys(styled_children[0].attr).filter(x => presentation.has(x)).forEach(y => {
+              rule.attr[y] = styled_children[0].attr[y]
+              delete styled_children[0].attr[y]
+            })
+          }
+        }
+
         symbols.push(rule)
         processed++
         return file.writeAsync(doc.toString({ compressed: true }) + '\n')
@@ -68,18 +170,31 @@ glob.globAsync(argv.source, { nocase: true }).filter(x => x !== svgout).map(fnam
 })
 .then(() => {
   file.end('</svg>')
+  // create and optimize css
   let style = ''
-  while (symbols.length > 0) {
-    let {width, height} = symbols[0]
-    let group = symbols.filter(x => x.width === width && x.height === height).map(x => x.name)
-    symbols = symbols.filter(x => x.width !== width || x.height !== height)
-    style +=
-`.${group.join(',\n.')} {
-  width: ${width};
-  height: ${height};
-}
-`
-  }
+  const wh = ['width', 'height']
+  symbols.forEach((symb, index) => {
+    if (Object.keys(symb.attr).length === 0) return
+    const rest_symbols = symbols.slice(index)
+    let attrs = Object.assign({}, symb.attr)
+    let same_size = rest_symbols.filter(x => lookslike(attrs, x.attr, wh))
+    let same_style = rest_symbols.filter(x => lookslike(attrs, x.attr))
+    if (same_size.length > same_style.length) {
+      same_size = same_size.map(x => {
+        deletelike(x.attr, wh)
+        return x.name
+      })
+      let { width, height } = attrs
+      style += style_format(same_size, { width, height })
+      if (Object.keys(symb.attr).length > 0) {
+        attrs = Object.assign({}, symb.attr)
+        same_style = rest_symbols.filter(x => equals(attrs, x.attr))
+        style += wipe_style_format(same_style, attrs)
+      }
+    } else {
+      style += wipe_style_format(same_style, attrs)
+    }
+  })
   fs.writeFileAsync(cssout, style, encoding)
     .then(() => console.log(`Successfully processed files: ${processed}/${total}.`))
 })
